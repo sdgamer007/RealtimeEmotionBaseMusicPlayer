7168995fd498e0f17285bf96743413ebe1eafa91
sdgamer007 <sohaildua38@gmail.com>
2020-05-26 17:02:24 +0200

    Dividing data into training and test cases

--- emotionDetection.py
+++ emotionDetection.py
@@ -15,8 +15,34 @@ from keras.utils import np_utils
 
 df = pd.read_csv('Data\\fer2013.csv')
 
-print(df.info)
+X_train,train_y,X_test,test_y =[],[],[],[]
 
-print(df["Usage"].value_counts())
+for index ,row in df.iterrows():
+    
+    row_data = row['pixels'].split(" ")
+    try:
+        if 'Training' in row['Usage']:
+            X_train.append(np.array(row_data, 'float32'))
+            train_y.append(row['emotion'])
+        
+        elif 'PublicTest' in row['Usage']:
+            X_test.append(np.array(row_data,'float32'))
+            test_y.append(row['emotion'])
+    except:
+        print(f"error occured at index :{index} and row:{row}")
 
-print(df.head())
+X_train = np.array(X_train,'float32')
+train_y = np.array(train_y,'float32')
+X_test = np.array(X_test,'float32')
+test_y = np.array(test_y,'float32')       
+        
+
+#normalizing data between oand 1
+X_train -= np.mean(X_train, axis=0)
+X_train /= np.std(X_train, axis=0)
+
+X_test -= np.mean(X_test, axis=0)
+X_test /= np.std(X_test, axis=0)
+
+        
+        
